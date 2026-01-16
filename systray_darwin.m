9483f6fb47380e61a9cfead3438ff9120ff6b1e3
Gabe Cook <git@gabe565.com>
2026-01-16 04:30:33 -0600

    fix: icon not highlighted while menu is open on macos

--- systray_darwin.m
+++ systray_darwin.m
@@ -289,9 +289,11 @@ NSMenuItem *find_menu_item(NSMenu *ourMenu, NSNumber *menuId) {
 
 - (void)show_menu
 {
+  self->statusItem.button.highlighted = YES;
   [self->menu popUpMenuPositioningItem:nil
                             atLocation:NSMakePoint(0, 0)
                                 inView:self->statusItem.button];
+  self->statusItem.button.highlighted = NO;
 }
 
 - (void) show_menu_item:(NSNumber*) menuId
